6ba4eb26e92b0d2389c4c51eea8e059ceb9672de
Vedant Pawar <vedantpawar896@gmail.com>
2026-09-07 11:14:20 -0400

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.cpp
@@ -0,0 +1,26 @@
+#include<cstring>
+class Solution {
+public:
+    int memo[1000][1000];
+    int dp(int i,int j,string& text1,string& text2) {
+        if(i==text1.size() || j==text2.size()) {
+            return 0;
+        }
+
+        if(memo[i][j] != 0) {
+            return memo[i][j];
+        }
+
+        if(text1[i]==text2[j]) {
+            memo[i][j] += dp(i+1,j+1,text1,text2)+1;
+        }else {
+            memo[i][j] += max(dp(i+1,j,text1,text2),dp(i,j+1,text1,text2));
+        }
+        return memo[i][j];
+    }
+    
+    int longestCommonSubsequence(string text1, string text2) {
+        memset(memo,0,sizeof(memo));
+        return dp(0,0,text1,text2);
+    }
+};
